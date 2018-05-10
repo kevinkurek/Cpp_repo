78a89016c48df02c7fa9ce0daf7949a0d504fcee
Kevin Kurek <kevinkurek@live.com>
2018-05-09 22:27:07 -0700

    Adding file

---
+++ HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Kevin Kurek on 5/9/18.
+//  Copyright © 2018 Kevin Kurek. All rights reserved.
+//
+
+#include <iostream>
+
+int main()
+{
+    int x = 6;
+    int y = 10;
+    int sum = x + y;
+    std::cout << "Hello, World!\n";
+    std::cout << sum;
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 6;
+    int b = 57;
+    int sum = x + b;
+    cout << sum;
+
+
+}
