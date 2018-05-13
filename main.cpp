8472ad34e883d2ccdb433f526f3e15c5f8570d12
Kevin Kurek <kevinkurek@live.com>
2018-05-12 23:14:10 -0700

    Delete HelloWorld Folder

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
-using namespace std;
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
 
 int main()
 {
     int x = 6;
-    int b = 57;
-    int sum = x + b;
-    cout << sum;
-
-
+    int y = 10;
+    int sum = x + y;
+    std::cout << "Hello, World!\n";
+    std::cout << "Total Sum: " << sum << "\n";
+    return 0;
 }
